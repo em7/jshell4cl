51361adae3cc8fb5dc48baba695eaa0e60827436
em7 <mm007.emko@gmail.com>
2023-06-17 11:21:17 +0200

    run tests lisp loaded from ql

--- run-tests.lisp
+++ run-tests.lisp
@@ -1,6 +1,7 @@
 (load "quicklisp.lisp")
 (quicklisp-quickstart:install)
 (push (uiop:getcwd) asdf:*central-registry*)
+(ql:quickload :jshell4cl)
 (asdf:load-system :jshell4cl)
 (asdf:test-system :jshell4cl)
 
