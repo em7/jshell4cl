920515d7165b186eb2bc13852cc062b2b1ca7d63
em7 <mm007.emko@gmail.com>
2023-06-17 11:25:47 +0200

    ql tests to download dependencies

--- run-tests.lisp
+++ run-tests.lisp
@@ -1,7 +1,10 @@
 (load "quicklisp.lisp")
 (quicklisp-quickstart:install)
 (push (uiop:getcwd) asdf:*central-registry*)
+
 (ql:quickload :jshell4cl)
+(ql:quickload :jshell4cl/tests)
+
 (asdf:load-system :jshell4cl)
 (asdf:test-system :jshell4cl)
 
